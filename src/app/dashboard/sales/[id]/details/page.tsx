86e2dadb482f987d471d410f4d8d339d05659594
aldisypu <aldisypu@gmail.com>
2024-09-17 15:56:38 +0700

    Fetching Data and Update sidenav

---
+++ src/app/dashboard/sales/[id]/details/page.tsx
@@ -0,0 +1,136 @@
+import Link from "next/link";
+
+interface Details {
+    id: string;
+    sale_id: string;
+    product_id: string;
+    quantity: number;
+    price: number;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/sales/${params.id}/details`, { cache: 'no-store' })
+    const response = await data.json()
+    const details: Details[] = response.data
+    return (
+        <>
+        <p>Details</p>
+            <div dir="ltr" className="rounded-lg border border-gray-200 dark:border-gray-700">
+                <div className="overflow-x-auto rounded-t-lg">
+                    <table
+                    className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm dark:divide-gray-700 dark:bg-gray-900"
+                    >
+                    <thead className="ltr:text-left rtl:text-right">
+                        <tr>
+                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            Product ID
+                        </th>
+                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            Quantity
+                        </th>
+                        <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            Price
+                        </th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                        {details.map((detail) => (
+                        <tr key={detail.id}>
+                        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            {detail.product_id}
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            {detail.quantity}
+                        </td>
+                        <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                            {detail.price}
+                        </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                </div>
+
+                <div className="rounded-b-lg border-t border-gray-200 px-4 py-2 dark:border-gray-700">
+                    <ol className="flex justify-end gap-1 text-xs font-medium">
+                    <li>
+                        <Link
+                        href="#"
+                        className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180 dark:border-gray-800 dark:bg-gray-900 dark:text-white"
+                        >
+                        <span className="sr-only">Prev Page</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-3"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                            fillRule="evenodd"
+                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                            />
+                        </svg>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                        href="#"
+                        className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900 dark:border-gray-800 dark:bg-gray-900 dark:text-white"
+                        >
+                        1
+                        </Link>
+                    </li>
+
+                    <li
+                        className="block size-8 rounded border-blue-600 bg-blue-600 text-center leading-8 dark:text-white"
+                    >
+                        2
+                    </li>
+
+                    <li>
+                        <Link
+                        href="#"
+                        className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900 dark:border-gray-800 dark:bg-gray-900 dark:text-white"
+                        >
+                        3
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                        href="#"
+                        className="block size-8 rounded border border-gray-100 bg-white text-center leading-8 text-gray-900 dark:border-gray-800 dark:bg-gray-900 dark:text-white"
+                        >
+                        4
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                        href="#"
+                        className="inline-flex size-8 items-center justify-center rounded border border-gray-100 bg-white text-gray-900 rtl:rotate-180 dark:border-gray-800 dark:bg-gray-900 dark:text-white"
+                        >
+                        <span className="sr-only">Next Page</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="size-3"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                            fillRule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clipRule="evenodd"
+                            />
+                        </svg>
+                        </Link>
+                    </li>
+                    </ol>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import { NavLinks } from "./nav-links";
+import { NavLinks } from "@/app/ui/dashboard/nav-links";
 
 export default function SideNav() {
     return (
@@ -11,50 +10,6 @@ export default function SideNav() {
 
                 <ul className="mt-6 space-y-1">
                     <NavLinks />
-                    <li>
-                        <details className="group [&_summary::-webkit-details-marker]:hidden">
-                        <summary
-                            className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                        >
-                            <span className="text-sm font-medium"> Teams </span>
-
-                            <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="size-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                fillRule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clipRule="evenodd"
-                                />
-                            </svg>
-                            </span>
-                        </summary>
-
-                        <ul className="mt-2 space-y-1 px-4">
-                            <li>
-                            <Link
-                                href="#"
-                                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                            >
-                                Banned Users
-                            </Link>
-                            </li>
-
-                            <li>
-                            <Link
-                                href="#"
-                                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                            >
-                                Calendar
-                            </Link>
-                            </li>
-                        </ul>
-                        </details>
-                    </li>
                 </ul>
             </div>
             <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
